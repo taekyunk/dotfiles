2b30c5f0142bfca3e0650eb47d50a8ccc89f44d8
Taekyun Kim <taekyunk@gmail.com>
2015-03-15 18:20:31 -0400

    do not ask questions when adding repository

--- install_prog.sh
+++ install_prog.sh
@@ -1,6 +1,6 @@
 # Assumes that 'git' is installed already
 # double commander
-sudo add-apt-repository ppa:alexx2000/doublecmd
+sudo add-apt-repository ppa:alexx2000/doublecmd -y
 sudo apt-get update
 sudo apt-get install doublecmd-gtk -y
 # vim
